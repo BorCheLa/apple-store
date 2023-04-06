3caebf521516f8ce6b0f09f12d369afad1e8458f
b.chebotarev <adebos@yandex.ru>
2023-04-06 08:12:14 +0300

    add reviews

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,12 @@ introMoreButton.addEventListener(`click`, (event) => {
   event.target.remove();
   introTextWrapper.classList.add(`full`);
 });
+
+const reviewMoreButtons = document.querySelectorAll(`.reviewMoreButton`);
+reviewMoreButtons.forEach((moreButton) => {
+  moreButton.addEventListener(`click`, (event) => {
+    const parent = event.target.parentElement;
+    event.target.remove();
+    parent.classList.add(`full`);
+  });
+});
